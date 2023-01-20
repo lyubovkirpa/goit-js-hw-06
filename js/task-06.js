305cc538375eb0e039803104d6c82686327f196e
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-01-21 00:02:40 +0200

    task-06 add

--- js/task-06.js
+++ js/task-06.js
@@ -3,12 +3,11 @@ const inputText = document.querySelector("input");
 inputText.addEventListener("blur", onInputTextBlur);
 
 function onInputTextBlur(event) {
-  if (event.currentTarget.length > inputText.dataset.length) {
+  if (event.currentTarget.value.length > inputText.dataset.length) {
     inputText.classList.add("invalid");
     inputText.classList.remove("valid");
   } else {
     inputText.classList.remove("invalid");
     inputText.classList.add("valid");
   }
-  // console.log(event.currentTarget.length);
 }
