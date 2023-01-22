ffe85de71af17d5011d25a87276d5b1c858a9e18
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-01-23 00:31:09 +0200

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const body = document.body;
+const colorBtn = document.querySelector("button");
+const span = document.querySelector("span");
+
+colorBtn.addEventListener("click", onColorBtnClick);
+
+function onColorBtnClick() {
+  body.style.backgroundColor = getRandomHexColor();
+  span.textContent = getRandomHexColor();
+}
