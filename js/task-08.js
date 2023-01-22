d485abbea01e769647fb9075e7f8a10252ed5c0c
Lyubov Kirpa <kirpa.lyubov.a@gmail.com>
2023-01-22 23:34:30 +0200

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,18 @@
+const form = document.querySelector("form");
+form.addEventListener("submit", onFormSabmit);
 
+function onFormSabmit(event) {
+  event.preventDefault();
+
+  const formElements = event.currentTarget.elements;
+
+  const mail = formElements.email.value;
+  const password = formElements.password.value;
+
+  if (mail === "" || password === "") {
+    return console.log(alert);
+  }
+
+  console.log(`mail: ${mail}, password: ${password}`);
+  event.currentTarget.reset();
+}
